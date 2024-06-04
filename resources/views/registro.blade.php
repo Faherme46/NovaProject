c6ee36f261c88c060b9b80faf16054529c382663
Faherme46 <faherme46@gmail.com>
2024-06-04 11:51:47 -0500

    Creo vista

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,3 +1,35 @@
-<div>
-    He who is contented is rich. - Laozi 
-</div>
+@extends('layout.app')
+
+
+
+@section('content')
+
+    <link rel="stylesheet" href="assets/css/registro.css">
+    <h1>Registro</h1>
+
+
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <script src="assets/js/registro.js"></script>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,18 @@
 @extends('layout.app')
 
 @section('content')
-    <a href="registro">Registro</a>
+    <link rel="stylesheet" href="public/assets/css/welcome.css">
 
-    
+
+    <a href="registro">Registro</a>
+    <br>
+    <a href="login">login</a> <br>
+    <a href="crearusuarios">crearusuarios</a> <br>
+    <a href="preguntas">preguntas</a>
+
+
+
+
 
 
 @endsection
