5df290f5672a693f047e39c89a282a3da243a37d
Faherme46 <faherme46@gmail.com>
2025-02-27 21:49:54 -0500

    12112

--- resources/views/views/elecciones/terminal.blade.php
+++ resources/views/views/elecciones/terminal.blade.php
@@ -2,17 +2,17 @@
 
     <x-alerts />
 
-    <p class="d-none " wire:poll.1000ms='verifyAsistente'></p>
-    @if ($asistente && $control)
+    <p class="d-none " wire:keypress='verifyAsistente'></p>
+    @if ($asistente && $control && !$asamblea['h_cierre'])
         <div class="d-flex justify-content-center vh-100 align-items-center">
             <div class="card">
                 <div class="card-body">
                     <h1 class="mb-0 card-title " style="font-size: 4rem">{{ $asistente->fullName() }}</h1>
-                    <h1 class="mb-0 text-muted"> TORRE {{ strtoupper($control->vote) }} </h1>
+                    <h1 class="mb-0 text-muted"> {{ strtoupper($control->vote) }} </h1>
                 </div>
                 <div class="card-footer d-flex justify-content-end">
 
-                    <button class="btn btn-primary fs-3" wire:click='votar' >VOTAR</button>
+                    <button class="btn btn-primary fs-3" wire:click='votar'>SIGUIENTE</button>
                 </div>
             </div>
 
@@ -23,7 +23,12 @@
                 <img src="/storage/images/logo.png" style="width: 400px;" alt="logo">
                 <h1 class="mb-0 me-3 mt-5 "
                     style="font-size: 6rem;color:#cc0000; font-weight:bolder;font-family:'Trebuchet MS';">
-                    {{ strtoupper($terminal->user_name) }}
+                    @if ($asamblea['h_cierre'])
+                        VOTACIONES CERRADAS
+                    @else
+                        {{ strtoupper($terminal->user_name) }} <br>
+                        <button class="btn btn-"></button>
+                    @endif
                 </h1>
             </div>
         </div>
