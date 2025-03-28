ada8f000bfde46c2c8f823121f7460eaa4b69866
Faherme46 <faherme46@gmail.com>
2025-03-27 21:38:36 -0500

    export

--- app/Exports/PrediosControlExport.php
+++ app/Exports/PrediosControlExport.php
@@ -9,7 +9,7 @@ class PrediosControlExport implements FromArray
 {
     public function array(): array
     {
-        $array = Predio::select(['id','control_id'])->orderBy('id')->get()->toArray();
+        $array = Predio::select(['id','descriptor1','numeral1','descriptor2','numeral2','coeficiente','control_id'])->orderBy('id')->get()->toArray();
 
         return $array;
     }
